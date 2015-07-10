47027fad2fc83cfd3aae0f663b256b28a3258ed6
Subash Adhikari <me@subash.com.au>
2015-07-10 15:20:51 +1000

    change syntax of for

--- init.sh
+++ init.sh
@@ -16,8 +16,7 @@ dotfiles[tmux.conf]=tmux/tmux.conf
 dotfiles[gitconfig]=git/gitconfig
 
 # load each dotfiles
-for i in "${!dotfiles[@]}"
-do
+for i in "${!dotfiles[@]}"; do
   config=~/.$i
   mv $config "$BACKUP_DIR" 2>/dev/null
   ln -s dotfiles/${dotfiles[$i]} $config
