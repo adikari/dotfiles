47ec8c92c019f9a813bd99ae73301a407246d0a8
Subash Adhikari <subash.adhikari@acer.edu.au>
2015-09-01 13:18:45 +1000

    update the script to have all functions in the same file

--- init.sh
+++ init.sh
@@ -1,25 +1,91 @@
-#!/bin/bash
+#!/bin/sh
 
-scripts=$HOME/dotfiles/scripts
+# color constants
+yellow='\033[0;33m'
+green='\033[0;32m'
+red='\033[0;31m'
+nc='\033[0m'
 
-. $scripts/utils/colors
-. $scripts/utils/backup_dir
+# create backup directory
+backup_dir="/tmp/dotfiles_$(date +%Y%m%d%H%M%S)"
+[ -d $backup_dir ] || mkdir "$backup_dir"
+echo -e "${green}Backup directory $backup_dir created.${nc}"
 
-# update configuration submodules
+bin=/usr/local/bin
+dotfiles=$HOME/dotfiles
+
+restart_required=false
+
+# create backup
+backup() {
+  if [ -f $config ]; then
+    mv $1 "$backup_dir" 2>/dev/null
+    echo -e "\n${yellow}$1 is backed up in "$backup_dir".${nc}"
+  fi
+}
+
+# check if given command is available
+has_command() {
+  command -v $1 >/dev/null 2>&1
+}
+
+# create symlink
+link() {
+  if ! diff $1 $2 &>/dev/null; then
+    backup $2; sudo ln -sf $1 $2 2>/dev/null
+
+    echo -e "\n${green}$2 is successfully linked.${nc}"
+
+    if [ "$2" == "zshrc" ]; then restart_required=true; fi
+  fi
+}
+
+# install given command if not already exists
+introduce() {
+  if has_command brew; then
+    brew install $1
+  elif has_command yum; then
+    sudo yum install $1
+  elif has_command apt-get; then
+    sudo apt-get install $1
+  fi
+}
+
+# change shell to zsh
+change_shell() {
+  has_command $1 || introduce $1
+
+  if [ $SHELL != $(which $1) ];then
+    echo -e "${yellow}For dotfiles to work, we need to change shell to zsh.${nc}"
+    chsh -s $(which $1)
+  fi
+}
+
+# setup phpcs
+setup_phpcs() {
+  phpcs=$dotfiles/phpcs/PHP_CodeSniffer
+
+  link $phpcs/scripts/phpcs $bin/phpcs
+  link $phpcs/scripts/phpcbf $bin/phpcbf
+
+  echo -e "\n${green}PHPCS is successfully set up.${nc}"
+}
+
+# # update configuration submodules
 git submodule init
 git submodule update --init --recursive
 
-source $scripts/change_shell
-source $scripts/link_dotfiles
+change_shell zsh
 
-source $scripts/install/vim
+link $dotfiles/zsh/zshrc $HOME/.zshrc
+link $dotfiles/vim/vimrc $HOME/.vimrc
+link $dotfiles/tmux/tmux.conf $HOME/.tmux.conf
 
-source $scripts/setup_phpcs
+setup_phpcs
 
 echo -e "\n${green}Hurray!!! Dotfiles successfully setup.${nc}\n"
 
 if [ "$restart_required" = true ]; then
-  echo -e "${red}You must restart your session in order for configurations to take affect!!!${nc}\n"
-  unset restart_required
+   echo -e "${red}You must restart your session in order for configurations to take affect!!!${nc}\n"
 fi
 
