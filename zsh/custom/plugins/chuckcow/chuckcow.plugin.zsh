9150f94731132fbbe528985ac9cf98c625c31f8a
Subash Adhikari <subash.adhikari@acer.edu.au>
2015-09-04 11:35:28 +1000

    dont check for command exists

--- zsh/custom/plugins/chuckcow/chuckcow.plugin.zsh
+++ zsh/custom/plugins/chuckcow/chuckcow.plugin.zsh
@@ -2,29 +2,14 @@
 
 # display random chuck norris quotes in cowthink
 
-has_command() {
-  command -v $1 >/dev/null 2>&1
-}
-
 current_dir=$(pwd)
-
-if ! has_command fortune; then
-  echo "fortune command is required for chuckcow to run.\n"
-fi
-
-if ! has_command cowsay; then
-  echo "cowsay command is required for chuckcow to run."
-fi
 
 chuckcow() {
   plugin="chucknorris"
 
   if [ -f $ZSH/plugins/$plugin/$plugin.plugin.zsh ]; then
-
-    if has_command cowsay && has_command fortune; then
-      source $ZSH/plugins/$plugin/$plugin.plugin.zsh
-      fortune -a $ZSH/plugins/chucknorris/fortunes | cowsay
-    fi
+    source $ZSH/plugins/$plugin/$plugin.plugin.zsh
+    fortune -a $ZSH/plugins/chucknorris/fortunes | cowsay
   fi
 }
 
