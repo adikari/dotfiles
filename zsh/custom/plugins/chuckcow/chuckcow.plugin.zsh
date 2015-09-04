fdb18770e8968d6191a024b90b9f52eaf6d5e1cd
Subash Adhikari <subash.adhikari@acer.edu.au>
2015-09-04 11:27:40 +1000

    update plugin to not install cowsay and fortune

--- zsh/custom/plugins/chuckcow/chuckcow.plugin.zsh
+++ zsh/custom/plugins/chuckcow/chuckcow.plugin.zsh
@@ -9,32 +9,11 @@ has_command() {
 current_dir=$(pwd)
 
 if ! has_command fortune; then
-  if has_command brew; then
-    brew install fortune
-  elif has_command yum; then
-    cd /tmp
-    wget http://download.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
-    rpm -ivh epel-release-6-8.noarch.rpm
-    sudo rm -rf epel*
-    sudo yum install fortune-mod.x86_64
-    cd $current_dir
-  elif has_command apt-get; then
-    sudo apt-get install fortune
-  fi
+  echo "fortune command is required for chuckcow to run.\n"
 fi
 
-# installs cow
-# to uninstall
-# 1. remove /usr/bin/cowsay /usr/bin/cowthink
-# 2. remove /usr/local/share/cows
-
 if ! has_command cowsay; then
-  cd /tmp
-  git clone https://github.com/schacon/cowsay.git
-  cd cowsay
-  sudo ./install.sh
-  sudo rm -rf /tmp/cowsay
-  cd $current_dir
+  echo "cowsay command is required for chuckcow to run."
 fi
 
 chuckcow() {
