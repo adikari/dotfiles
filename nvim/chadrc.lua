2884905a033af64a1a42ca32753e2e9118bfe1d1
subash <me@subash.com.au>
2023-06-26 10:41:25 +1000

    update nvim theme to gruvbox

--- nvim/chadrc.lua
+++ nvim/chadrc.lua
@@ -5,8 +5,8 @@ local M = {}
 local highlights = require "custom.highlights"
 
 M.ui = {
-  theme = "onedark",
-  theme_toggle = { "onedark", "one_light" },
+  theme = "gruvbox",
+  theme_toggle = { "gruvbox", "gruvbox_light" },
 
   hl_override = highlights.override,
   hl_add = highlights.add,
