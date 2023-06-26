d16081d391016a76fcdb7b133f75a170729de4fe
subash <me@subash.com.au>
2023-06-26 11:31:32 +1000

    add format on save

--- nvim/configs/null-ls.lua
+++ nvim/configs/null-ls.lua
@@ -22,7 +22,27 @@ local sources = {
   b.formatting.clang_format,
 }
 
+-- format on save
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+local on_attach = function(client, bufnr)
+  if client.supports_method("textDocument/formatting") then
+    vim.api.nvim_clear_autocmds({
+      group = augroup,
+      buffer = bufnr,
+    })
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      group = augroup,
+      buffer = bufnr,
+      callback = function()
+        vim.lsp.buf.format({ bufnr = bufnr })
+      end,
+    })
+  end
+end
+
+
 null_ls.setup {
   debug = true,
   sources = sources,
+  on_attach = on_attach
 }
