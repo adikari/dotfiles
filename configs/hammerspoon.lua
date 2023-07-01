0b33f919fa5fe173441b49dfb7ab98708caab02b
subash <me@subash.com.au>
2023-07-02 00:03:49 +1000

    fix init script

---
+++ configs/hammerspoon.lua
@@ -0,0 +1,14 @@
+hs.hotkey.bind({ "ctrl" }, "space", function()
+	local app = hs.application.get("alacritty")
+	if app then
+		if not app:mainWindow() then
+			app:selectMenuItem({ "alacritty", "New OS window" })
+		elseif app:isFrontmost() then
+			app:hide()
+		else
+			app:activate()
+		end
+	else
+		hs.application.launchOrFocus("alacritty")
+	end
+end)
