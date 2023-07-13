de0a07a4884887ef5fc2757255964b6edb1cfe8d
subash <me@subash.com.au>
2023-07-13 20:10:49 +1000

    open nvim tree on vim startup

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,10 @@
 -- disable netrw plugin. see :help nvim-tree-netrw
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
+
+-- open nvim tree on startup
+vim.api.nvim_create_autocmd({ "VimEnter" }, {
+	callback = function()
+		require("nvim-tree.api").tree.open()
+	end,
+})
