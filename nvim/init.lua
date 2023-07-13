72a19d513f5e2e2a3bceacf109ccf11709ef0298
subash <me@subash.com.au>
2023-07-13 21:26:19 +1000

    disable opening nvim tree by default

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,8 +3,8 @@ vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
 -- open nvim tree on startup
-vim.api.nvim_create_autocmd({ "VimEnter" }, {
-	callback = function()
-		require("nvim-tree.api").tree.open()
-	end,
-})
+-- vim.api.nvim_create_autocmd({ "VimEnter" }, {
+-- 	callback = function()
+-- 		require("nvim-tree.api").tree.open()
+-- 	end,
+-- })
