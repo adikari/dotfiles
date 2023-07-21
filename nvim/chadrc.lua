a9a25682c82cb4ecee8ed598fcc889e7f86bf272
subash <me@subash.com.au>
2023-07-21 15:44:47 +1000

    add catpuccine theme

--- nvim/chadrc.lua
+++ nvim/chadrc.lua
@@ -5,8 +5,8 @@ local M = {}
 local highlights = require "custom.highlights"
 
 M.ui = {
-  theme = "gruvbox",
-  theme_toggle = { "gruvbox", "gruvbox_light" },
+  theme = "catppuccin",
+  theme_toggle = { "catppuccin", "gruvbox" },
 
   hl_override = highlights.override,
   hl_add = highlights.add,
