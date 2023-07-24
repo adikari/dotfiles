ebfba1f9c5f0b105bc084427c84f9d2d1560f923
subash <me@subash.com.au>
2023-07-24 11:46:34 +1000

    install plugins

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -1,4 +1,19 @@
-plugins_dir=$HOME/dotfiles//zsh/plugins
+plugins_dir=$HOME/dotfiles/zsh/plugins
+
+## start powerlevel 10k setup
+powerlevel10k_dir=$HOME/powerlevel10k
+if [ ! -d $powerlevel10k_dir ]; then
+  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $powerlevel10k_dir
+fi
+
+if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+fi
+
+source $HOME/powerlevel10k/powerlevel10k.zsh-theme
+source $HOME/dotfiles/zsh/p10k.zsh
+## end powerlevel 10k setup
+
 source $plugins_dir/z/zsh-z.plugin.zsh
-source $plugins_dir/git.plugin.zsh
-source $plugins_dir/github.plugin.zsh
+source $plugins_dir/git/git.plugin.zsh
+source $plugins_dir/github/github.plugin.zsh
