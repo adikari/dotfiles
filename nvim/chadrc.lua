1987fda95ac6be28ab1981080f529554efb2bf3e
subash adhikari <me@subash.com.au>
2023-07-24 20:22:57 +1000

    markdown preview (#5)
    * remove cow say
    
    * install markdown preview plugin
    
    * update readme
    
    * fix fzf key binding
    
    * customise powerlevel 10 k
    
    * switch vim status line
    
    * check before loading completions
    
    * fix icon for tmux statusline

--- nvim/chadrc.lua
+++ nvim/chadrc.lua
@@ -10,6 +10,10 @@ M.ui = {
 
   hl_override = highlights.override,
   hl_add = highlights.add,
+
+  statusline = {
+    theme = "vscode_colored",
+  },
 }
 
 M.plugins = "custom.plugins"
--- zsh/completions.zsh
+++
@@ -1,6 +0,0 @@
-load_completion() {
-  $1 completion zsh > "${ZSH_COMPLETION_DIR}/_$1"
-}
-
-load_completion safebox
-load_completion op
