123a11f2923cb92fcab49a42aca3fe0bcd5e0452
subash <me@subash.com.au>
2023-07-24 23:14:35 +1000

    add gh completion

---
+++ zsh/completions.zsh
@@ -0,0 +1,18 @@
+[ ! -d $ZSH_COMPLETION_DIR ] && mkdir -p $ZSH_COMPLETION_DIR
+fpath=($ZSH_COMPLETION_DIR $fpath)
+
+completions=(
+  safebox
+  op
+  docker
+)
+
+# load completion files
+for i ("$completions[@]"); do 
+  (( ${+commands[$i]} )) && $i completion zsh > "${ZSH_COMPLETION_DIR}/_$i"
+done
+
+gh completion -s zsh > "${ZSH_COMPLETION_DIR}/_gh"
+
+autoload -U compinit; compinit
+
