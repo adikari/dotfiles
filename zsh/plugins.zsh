abb4cf725406fdb19522f3d7348999c20dc37beb
subash adhikari <me@subash.com.au>
2023-07-24 23:14:54 +1000

    add gh completion (#7)

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -1,21 +1,5 @@
 plugins_dir=$HOME/dotfiles/zsh/plugins
 
-[ ! -d $ZSH_COMPLETION_DIR ] && mkdir -p $ZSH_COMPLETION_DIR
-fpath=($ZSH_COMPLETION_DIR $fpath)
-
-completions=(
-  safebox
-  op
-  docker
-)
-
-# load completion files
-for i ("$completions[@]"); do 
-  (( ${+commands[$i]} )) && $i completion zsh > "${ZSH_COMPLETION_DIR}/_$i"
-done
-
-autoload -U compinit; compinit
-
 source $plugins_dir/z/zsh-z.plugin.zsh
 source $plugins_dir/git/git.plugin.zsh
 source $plugins_dir/github/github.plugin.zsh
