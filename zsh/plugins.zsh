a879e5e7c9c85b5b1d90828ee02a5a6ef923cadd
subash <me@subash.com.au>
2023-07-31 14:36:07 +1000

    add gunstage

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -1,6 +1,7 @@
 plugins_dir=$HOME/dotfiles/zsh/plugins
 
 source $plugins_dir/z/zsh-z.plugin.zsh
+source $plugins_dir/gunstage/gunstage.plugin.zsh
 source $plugins_dir/git/git.plugin.zsh
 source $plugins_dir/github/github.plugin.zsh
 source $plugins_dir/autosuggestions/plugin.zsh
---
+++ zsh/plugins/gunstage/gunstage.plugin.zsh
@@ -0,0 +1,28 @@
+#!/usr/bin/env sh
+#
+#                   ╭╮
+#                  ╭╯╰╮
+#      ╭──┬╮╭┬─╮╭──┼╮╭┼──┬──┬──╮
+#      │╭╮││││╭╮┤ ─┤│││╭╮│╭╮││─┤
+#      │╰╯│╰╯│││├──││╰┤╭╮│╰╯││─┤
+#      ╰─╮├──┴╯╰┴──╯╰─┴╯╰┴─╮├──╯
+#      ╭─╯│              ╭─╯│
+#      ╰──╯              ╰──╯
+#
+#
+#      gunstage
+#      🔫 `git unstage` as a service
+#      author: Lucas Larson
+#      license: GPLP 1.0 or later (GPL-compatible)
+#      https://github.com/LucasLarson/gunstage
+#
+
+# enable `git unstage` in addition to `gunstage`
+# by adding `bin` directory to `PATH`
+# without adding an initial colon `:` to `PATH`
+# https://unix.stackexchange.com/a/415028
+PATH="${PATH:+${PATH}:}$(command dirname "$0")/bin"
+export PATH
+
+# keep backwards compatibility
+alias gunstage='git-unstage'
