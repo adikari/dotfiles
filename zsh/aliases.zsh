06c9b8e67a862e0f9f0a1bc62e83ff3c08467935
subash <me@subash.com.au>
2023-08-02 20:37:24 +1000

    add koba vpn script

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,9 +2,16 @@ alias ss="sudo su -"
 alias tmux="tmux -2"
 
 alias fucking=sudo
+
 alias av=aws-vault
+alias ave="aws-vault exec"
 
 alias vim='nvim'
 alias vi="nvim"
 
 alias pn="pnpm"
+
+alias vpn="openvpn"
+
+alias aws-koba-playground='aws-google-auth -p koba-playground; export AWS_PROFILE=koba-playground;'
+alias aws-koba-dev='aws-google-auth -p koba-dev; export AWS_PROFILE=koba-dev;'
