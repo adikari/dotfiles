3855b47931405f1e93f20e233689870e5c7e01c8
subash <me@subash.com.au>
2023-11-10 18:29:49 +1100

    add alias for notes

--- config/zsh/aliases.zsh
+++ config/zsh/aliases.zsh
@@ -15,6 +15,7 @@ alias \
   lynx="lynx -cfg=$XDG_CONFIG_HOME/lynx/lynx.cfg -lss=$XDG_CONFIG_HOME/lynx/lynx.lss" \
   abook="abook --config "$XDG_CONFIG_HOME"/abook/abookrc --datafile "$XDG_DATA_HOME"/abook/addressbook" \
   ikhal="ikhal -l ~/.local/var/khal/ikhal.log" \
+  notes="cat ~/note | glow" \
   aws-koba-playground='aws-google-auth -p koba-playground; export AWS_PROFILE=koba-playground;' \
   aws-koba-dev='aws-google-auth -p koba-dev; export AWS_PROFILE=koba-dev;'
 
