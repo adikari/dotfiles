84ddb40bf0637952dd5f9d07dc3fb7fb39084c42
subash <me@subash.com.au>
2025-07-01 16:08:39 +1000

    biome will auto organise imports on save

--- config/nvim/lua/configs/conform.lua
+++ config/nvim/lua/configs/conform.lua
@@ -1,8 +1,7 @@
 local conform = require "conform"
 
-local function biome_lsp_or_prettier(bufnr)
+local function biome_lsp_or_prettier()
   local has_prettier = vim.fs.find({
-    -- https://prettier.io/docs/en/configuration.html
     ".prettierrc",
     ".prettierrc.json",
     ".prettierrc.yml",
@@ -46,21 +45,24 @@ local options = {
     c = { "clang_format" },
     ["_"] = { "trim_whitespace" },
   },
+  formatters = {
+    biome = {
+      command = "biome",
+      args = { "check", "--write", "--unsafe", "$FILENAME" },
+      stdin = false,
+    },
+  },
   format_on_save = function(bufnr)
-    -- Disable autoformat on certain filetypes
     local ignore_filetypes = { "txt" }
     if vim.tbl_contains(ignore_filetypes, vim.bo[bufnr].filetype) then
       return
     end
 
-    -- Disable with a global or buffer-local variable
     if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
       return
     end
 
-    -- Disable autoformat for files in a certain path
     local ignore_paths = { "/node_modules/", "/.sst/", "/.next/" }
-
     local isIgnoredPath = vim.tbl_contains(ignore_paths, function(path)
       return vim.api.nvim_buf_get_name(bufnr):match(path)
     end)
