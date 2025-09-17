dc1cf6173875a9f9f41d88011f72440f122fcca2
subash <me@subash.com.au>
2025-09-17 13:22:32 +1000

    upgrade to omarchy v3.0

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -29,6 +29,7 @@ require("lazy").setup({
 }, lazy_config)
 
 -- load theme
+dofile(vim.g.base46_cache .. "syntax")
 dofile(vim.g.base46_cache .. "defaults")
 dofile(vim.g.base46_cache .. "statusline")
 
