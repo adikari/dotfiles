39637dfcc02e60a550e6883f740e66fd5a08d7dc
subash <me@subash.com.au>
2025-12-02 12:02:03 +1100

    remove leap plugin

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -99,14 +99,6 @@ return {
   },
 
   {
-    "ggandor/leap.nvim",
-    event = "VeryLazy",
-    config = function()
-      require("leap").add_default_mappings(true)
-    end,
-  },
-
-  {
     "kylechui/nvim-surround",
     event = "VeryLazy",
     config = function()
